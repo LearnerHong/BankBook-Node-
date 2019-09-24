dffde748a15c8319872567ab852fa008beafe1a3
ZhiHong_Xu <1762704677@qq.com>
2019-09-24 11:12:42 +0800

    提交node依赖包

--- README.md
+++
@@ -1,1 +0,0 @@
-### BankBook项目的后端代码，用nodejs语言实现
--- model/MsgList.js
+++
@@ -1,7 +0,0 @@
-const mongoose =require('mongoose');
-const MsgListSchema = new mongoose.Schema({
-     time:String,
-	 content:String,//发表的内容
-	 refUser:String//用于关联用户id，也就是看这条说说是谁发表的
-    });
-module.exports = mongoose.model('MsgList', MsgListSchema);
